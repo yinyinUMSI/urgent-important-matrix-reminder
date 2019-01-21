929b22031c84fd67032d3c183c5515efa549d1ba
yinyinflorence <869115528@qq.com>
2019-01-21 01:55:27 -0500

    first iteration finish

--- js/indexedDB.js
+++ js/indexedDB.js
@@ -1,4 +1,63 @@
-document.write("<script type='text/javascript' src='show.js'></script>");
+let status = 0;
+
+function addButtonAnimation() {
+  if (status == 0) {
+    addTurn();
+    showColumn();
+    status = (status+1)%2;
+  }
+  else {
+    makeSureDelete(); 
+  }
+}
+
+function hideColumn() {
+  document.getElementById("notShow").style.display="none";
+}
+
+function addTurnBack() {
+  var elem = document.getElementById("addButton");
+  var ang = -46;
+  var id = setInterval(frame,3);
+  function frame() {
+    if (ang == 0) {
+      clearInterval(id);
+    }
+    else {
+      ang = ang+2;
+      elem.style.transform = "rotate("+ ang +"deg)";
+    }
+  }
+}
+
+
+function showColumn() {
+  document.getElementById("notShow").style.display="block";
+}
+function addTurn() {
+  var elem = document.getElementById("addButton");
+  var ang = 0;
+  var id = setInterval(frame,3);
+  function frame() {
+    if (ang == -46) {
+      clearInterval(id);
+    }
+    else {
+      ang = ang-2;
+      elem.style.transform = "rotate("+ ang +"deg)";
+    }
+  }
+}
+
+function makeSureDelete() {
+  if (confirm("All your input will not be saved")) {
+    addTurnBack();
+    hideColumn();
+    clearForm();
+    status = (status+1)%2;
+  }
+}
+
 let dbId = 1;
 let cateArr = [0,0,0,0];
 
@@ -9,8 +68,13 @@ function clickCompleteEvent() {
 		status = (status+1)%2;
 		let formObj = getFormValue();
 		console.log(formObj);
-		addToDatabase(formObj);		
+		addToDatabase(formObj);	
+    clearForm();	
 	}
+}
+
+function clearForm() {
+  document.getElementById("Form").reset();
 }
 
 function formValidation() {
